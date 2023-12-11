5b33c1c9c1a331a478bdae23a539dc1f1c2ace9d
sarah2244-4 <douthwaite.sarah@yahoo.co.uk>
2023-12-11 16:44:27 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,15 @@
+import os 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h1>Hello,</h1> <h2>World<h2>"
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"), 
+        port=int(os.environ.get("PORT", "5000")), 
+        debug=True # Remove debug=True when app is submitted or produced
+    )
